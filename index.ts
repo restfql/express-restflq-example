13968842593a34fbc8ac8ea6e3a9d887427e044e
kanekotic <me@kanekotic.com>
2023-03-10 08:36:49 +0100

    ALVARO | added intercept middleware

--- index.ts
+++ index.ts
@@ -2,6 +2,15 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
+app.use((req, res: any, next)=>{
+    var originalSend = res.send;
+    res.send = function(args: any){
+        console.log(args)
+        originalSend.apply(res,["something else"]);
+    };
+    next();
+})
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
