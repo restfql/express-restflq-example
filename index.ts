d4314bec922177a3a9111795a2afeede5531bd43
kanekotic <me@kanekotic.com>
2023-03-10 16:29:46 +0100

    ALVARO | filter in query

--- index.ts
+++ index.ts
@@ -1,18 +1,33 @@
 import express from 'express';
+import gql from 'graphql-tag';
 const app = express();
 const port = 3000;
 
+const filterResponse = (response: any, model: any, acc: any) =>{
+    for (const field of model.selectionSet.selections) {
+        acc[field.name.value] = field.selectionSet? filterResponse(response[field.name.value], field, {}) : response[field.name.value]
+    }
+    return acc
+}
+
 app.use((req, res: any, next)=>{
-    var originalSend = res.send;
-    res.send = function(args: any){
-        console.log(args)
-        originalSend.apply(res,["something else"]);
+    var originalJson = res.json;
+    res.json = (args: any) => {
+        const query = gql`${req.query.model}`
+        originalJson.apply(res,[filterResponse(args,query.definitions[0], {})]);
     };
     next();
 })
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.json({
+    message: 'Hello World!', 
+    description: "awesome description",
+    tags:{
+        mandatory: ["mand_tag1","mand_tag1"],
+        optional:  ["opt_tag1","opt_tag1"],
+    }
+});
 });
 
 app.listen(port, () => {
